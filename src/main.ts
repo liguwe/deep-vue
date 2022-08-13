46380e2f19be8cef017b3f74dd3befcaa5655b77
liguangwei <liguangwei@jwzg.com>
2022-08-13 08:47:35 +0800

    feat: 测试vscode 为啥解决冲突不好使了

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
+const t9 = 1;
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
-
+const t7 = 1;
 const app = createApp(App);
-
+const t8 = 1;
 app.use(createPinia());
 app.use(router);
-
+const t6 = 1;
 
 // 832: 挂载应用
 app.mount("#app");
